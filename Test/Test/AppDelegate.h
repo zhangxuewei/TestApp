83ae5390570922e5dc5cf42a2c0a114961fcbb1f
zhxuewei <303453384@qq.com>
2016-07-18 10:59:06 +0800

    初始化项目

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by aaron.wu on 16/7/18.
+//  Copyright © 2016年 xuewei.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by aaron.wu on 16/7/18.
+//  Copyright © 2016年 xuewei.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
